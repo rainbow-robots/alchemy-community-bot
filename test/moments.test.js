4d7726d247a577eddc0452d1a1121f8082fc442c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-29 16:45:35 -0700

    moments routes and tests

--- test/moments.test.js
+++ test/moments.test.js
@@ -1,7 +1,7 @@
 const { getMoments, getAgent } = require('./data-helper');
 
-const request = require('supertest');
-const app = require('../lib/app');
+// const request = require('supertest');
+// const app = require('../lib/app');
 
 describe('MOMENTOUS OCCASION', () => {
   it('can make beautiful moments', () => {
@@ -26,13 +26,23 @@ describe('MOMENTOUS OCCASION', () => {
 
   it('can get all moments', () => {
     const moments = getMoments();
-    
+
     return getAgent()
       .get('/api/v1/moments')
       .then(res => {
         moments.forEach(moment => {
           expect(res.body).toContainEqual(moment);
         });
+      });
+  });
+
+  it('it can delete a moment', () => {
+    const moment = getMoments()[0];
+
+    return getAgent()
+      .delete(`/api/v1/moments/${moment._id}`)
+      .then(res => {
+        expect(res.body).toHaveProperty('text');
       });
   });
 });
