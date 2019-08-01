b37ea50714168cfbc57964529d1703ebf6fcda4f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-01 15:06:57 -0700

    I went through to clean up the code base again and take out a few unused lines

--- test/emoji.test.js
+++ test/emoji.test.js
@@ -1,9 +1,6 @@
 const { getAgent } = require('./data-helper');
-const request = require('supertest');
-const app = require('../lib/app');
 
 describe('emoji neotrellis raspberry pi oh my tests', () => {
-  
   it('can tweet an emoji story', () => {
     return getAgent()
       .post('/api/v1/emojis')
@@ -12,8 +9,8 @@ describe('emoji neotrellis raspberry pi oh my tests', () => {
       })
       .then(res => {
         expect(res.body).toEqual({
-        _id: expect.any(String),
-        emojiStory: expect.any(String)
+          _id: expect.any(String),
+          emojiStory: expect.any(String)
         });
       });
   });
